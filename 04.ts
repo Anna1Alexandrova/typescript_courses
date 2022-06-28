778bc0253973a1649661d454e409f7219b28de96
Anna1Alexandrova <100306923+Anna1Alexandrova@users.noreply.github.com>
2022-06-28 12:01:31 +0300

    Delete 04.ts

--- 04.ts
+++
@@ -1,4 +0,0 @@
-// Создать переменную js, присвоить ей значение стоки "JavaScript". Вывести в консоль "I like Javascript".
-// Использовать строковую интерполяцию
-const js = 'JavaScript';
-console.log('I like ${js}');
