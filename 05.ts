225b7d1d27407988ce2dc6f2e44c76349c171d7d
Anna1Alexandrova <100306923+Anna1Alexandrova@users.noreply.github.com>
2022-06-28 12:01:41 +0300

    Delete 05.ts

--- 05.ts
+++
@@ -1,5 +0,0 @@
-// Доработать приложение. Создать еще одну переменную со значением "TypeScript". Вывести "I like Javascript and TypeScript".
-// Использовать строковую интерполяцию
-const js = 'JavaScript';
-const ts = 'TypeScript';
-console.log('I like ${js} and ${ts}');
